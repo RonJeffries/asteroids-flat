469919ef69f389fe05f5a2ccf65933c8f394a576
Ron Jeffries <ronjeffries@acm.org>
2023-02-11 09:26:56 -0500

    extract function for clarity.

--- src/main/kotlin/com/ronjeffries/flat/Game.kt
+++ src/main/kotlin/com/ronjeffries/flat/Game.kt
@@ -13,17 +13,21 @@ fun gameCycle(
 ) {
     for (spaceObject in spaceObjects) {
         if (spaceObject.type == SpaceObjectType.SHIP) {
-            if ( controls_left ) spaceObject.angle -= 250.0*deltaTime
-            if ( controls_right ) spaceObject.angle += 250.0*deltaTime
-            if (controls_accelerate) {
-                val deltaV = Vector2(120.0,0.0).rotate(spaceObject.angle) * deltaTime
-                spaceObject.dx += deltaV.x
-                spaceObject.dy += deltaV.y
-            }
+            applyControls(spaceObject, deltaTime)
         }
         move(spaceObject, width, height, deltaTime)
     }
     for (spaceObject in spaceObjects) {
         draw(spaceObject, drawer)
+    }
+}
+
+private fun applyControls(spaceObject: SpaceObject, deltaTime: Double) {
+    if (controls_left) spaceObject.angle -= 250.0 * deltaTime
+    if (controls_right) spaceObject.angle += 250.0 * deltaTime
+    if (controls_accelerate) {
+        val deltaV = Vector2(120.0, 0.0).rotate(spaceObject.angle) * deltaTime
+        spaceObject.dx += deltaV.x
+        spaceObject.dy += deltaV.y
     }
 }
