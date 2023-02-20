e45c0b1f473b3a3292374f9854dc92a4e6c0d67a
Ron Jeffries <ronjeffries@acm.org>
2023-02-20 15:09:26 -0500

    belay that order.

--- src/main/kotlin/com/ronjeffries/flat/Game.kt
+++ src/main/kotlin/com/ronjeffries/flat/Game.kt
@@ -124,7 +124,8 @@ fun createGame(missileCount: Int, asteroidCount: Int) {
 
 fun startGame(width: Int, height: Int) {
     Ship.active = true
-    setPosition(Ship, Vector2(width/2.0, height/2.0))
+    Ship.x = width/2.0
+    Ship.y = height/2.0
     activateAsteroids(4)
 }
 
