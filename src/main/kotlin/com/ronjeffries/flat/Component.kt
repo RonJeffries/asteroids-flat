f64fa79dda0b8664c6dfd75972915f78d66afc29
Ron Jeffries <ronjeffries@acm.org>
2023-03-05 14:17:19 -0500

    initial ActionObject test and class.

--- src/main/kotlin/com/ronjeffries/flat/Component.kt
+++ src/main/kotlin/com/ronjeffries/flat/Component.kt
@@ -9,3 +9,35 @@ data class Timer(override val entity: SpaceObject, val startTime: Double): Compo
 data class SaucerTimer(override val entity: SpaceObject): Component {
     var time = U.SaucerDelay
 }
+
+class PluggableTimer(
+    override val entity: SpaceObject,
+    private val startTime: Double,
+    val action: (PluggableTimer) -> Unit
+): Component {
+    var time = startTime
+    fun update(deltaTime: Double) {
+        time -= deltaTime
+        if (time <= 0.0 ) {
+            action(this)
+            time = startTime
+        }
+    }
+}
+
+class ActionTimer(
+    override val entity: SpaceObject,
+    val delayTime: Double,
+    val action: (ActionTimer) -> Unit
+): Component {
+    var time = delayTime
+    fun update(deltaTime: Double) {
+        if ( ! entity.active ) return
+        time -= deltaTime
+        if ( time <= 0.0) {
+            action(this)
+            time = delayTime
+        }
+    }
+
+}
--- src/test/kotlin/com/ronjeffries/flat/ComponentTests.kt
+++ src/test/kotlin/com/ronjeffries/flat/ComponentTests.kt
@@ -6,26 +6,17 @@ import org.openrndr.math.Vector2
 
 class ComponentTests {
     @Test
-    fun `saucer timer`() {
+    fun `saucer timer activates after SaucerDelay`() {
         val saucer = newSaucer()
-        saucer.position = Vector2(100.0, 100.0)
-        saucer.velocity = Vector2(0.0, 0.0)
-        val oldSpeed = saucerSpeed
+        val timer = SaucerTimer(saucer)
         assertThat(saucer.active)
             .describedAs("should start inactive").isEqualTo(false)
-        val timer = SaucerTimer(saucer)
         updateSaucerTimer(timer, 0.5)
         assertThat(saucer.active)
             .describedAs("should not activate immediately").isEqualTo(false)
         updateSaucerTimer(timer, U.SaucerDelay)
         assertThat(saucer.active)
             .describedAs("should start by now").isEqualTo(true)
-        assertThat(saucer.x)
-            .describedAs("should start on y axis").isEqualTo(0.0)
-        assertThat(saucer.dx)
-            .describedAs("should start at oldSpeed").isEqualTo(oldSpeed)
-        assertThat(saucerSpeed)
-            .describedAs("should negate saucerSpeed").isEqualTo(-oldSpeed)
     }
 
     @Test
@@ -45,5 +36,39 @@ class ComponentTests {
             .describedAs("speed should be oldSpeed").isEqualTo(oldSpeed)
         assertThat(saucerSpeed)
             .describedAs("should negate saucerSpeed").isEqualTo(-oldSpeed)
+    }
+
+    var executed = false
+    @Test
+    fun `pluggable timer triggers`() {
+        val entity = newAsteroid()
+        val timer = PluggableTimer(entity, 5.0) {timer: PluggableTimer -> executed = true}
+        executed = false
+        timer.update(4.0)
+        assertThat(executed)
+            .describedAs("not time yet")
+            .isEqualTo(false)
+        timer.update(1.0)
+        assertThat(executed)
+            .describedAs("should have triggered")
+            .isEqualTo(true)
+        assertThat(timer.time)
+            .describedAs("should reset timer")
+            .isEqualTo(5.0)
+    }
+
+    @Test
+    fun `action timer does not tick on inactive entity`() {
+        val entity = newAsteroid()
+        assertThat(entity.active)
+            .describedAs("initialized to false")
+            .isEqualTo(false)
+        executed = false
+        val timerTime = 1.0
+        val timer = ActionTimer(entity, timerTime) { executed = true}
+        timer.update(0.5)
+        assertThat(timer.time).isEqualTo(timerTime)
+            .describedAs("should be unchanged")
+            .isEqualTo(timerTime)
     }
 }
