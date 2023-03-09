205f5d47a8d50f58329059ed7015a44a60749601
Ron Jeffries <ronjeffries@acm.org>
2023-03-09 09:47:40 -0500

    move points and drawing to Drawing.kt.

--- src/main/kotlin/com/ronjeffries/flat/SpaceObject.kt
+++ src/main/kotlin/com/ronjeffries/flat/SpaceObject.kt
@@ -1,41 +1,6 @@
 package com.ronjeffries.flat
 
-import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.Drawer
-import org.openrndr.draw.isolated
 import org.openrndr.math.Vector2
-import kotlin.math.max
-import kotlin.random.Random
-
-private val shipPoints = listOf(
-    Vector2(-3.0, -2.0), Vector2(-3.0, 2.0), Vector2(-5.0, 4.0),
-    Vector2(7.0, 0.0), Vector2(-5.0, -4.0), Vector2(-3.0, -2.0)
-)
-
-private val shipFlare = listOf(
-    Vector2(-3.0,-2.0), Vector2(-7.0,0.0), Vector2(-3.0, 2.0)
-)
-
-private val asteroidPoints =
-    listOf(
-        Vector2(4.0, 2.0), Vector2(3.0, 0.0), Vector2(4.0, -2.0),
-        Vector2(1.0, -4.0), Vector2(-2.0, -4.0), Vector2(-4.0, -2.0),
-        Vector2(-4.0, 2.0), Vector2(-2.0, 4.0), Vector2(0.0, 2.0),
-        Vector2(2.0, 4.0), Vector2(4.0, 2.0),
-    )
-
-private val missilePoints =
-    listOf(Vector2(-1.0, -1.0), Vector2(-1.0, 1.0), Vector2(1.0, 1.0),
-        Vector2(1.0, -1.0), Vector2(-1.0, -1.0)
-    )
-
-private val saucerPoints = listOf(
-    Vector2(-2.0, -1.0), Vector2(2.0, -1.0), Vector2(5.0, 1.0),
-    Vector2(-5.0, 1.0), Vector2(-2.0, 3.0), Vector2(2.0, 3.0),
-    Vector2(5.0, 1.0), Vector2(2.0, -1.0), Vector2(1.0, -3.0),
-    Vector2(-1.0, -3.0), Vector2(-2.0, -1.0), Vector2(-5.0, 1.0),
-    Vector2(-2.0, -1.0)
-)
 
 val asteroidRadius = { asteroid: SpaceObject -> U.AsteroidKillRadius* asteroid.scale}
 val missileRadius = { _: SpaceObject -> U.MissileKillRadius }
@@ -107,32 +72,6 @@ fun deactivate(entity: SpaceObject) {
     }
 }
 
-fun draw(
-    spaceObject: SpaceObject,
-    drawer: Drawer,
-    deltaTime: Double,
-) {
-    drawer.isolated {
-        val scale = 4.0 *spaceObject.scale
-        drawer.translate(spaceObject.x, spaceObject.y)
-        drawer.scale(scale, scale)
-        drawer.rotate(spaceObject.angle)
-        drawer.stroke = ColorRGBa.WHITE
-        drawer.strokeWeight = 1.0/scale
-        shipSpecialHandling(spaceObject, drawer, deltaTime)
-        drawer.lineStrip(spaceObject.type.points)
-    }
-}
-
-private fun shipSpecialHandling(spaceObject: SpaceObject, drawer: Drawer, deltaTime: Double) {
-    if (spaceObject.type == SpaceObjectType.SHIP) {
-        dropScale = max(dropScale - U.ShipDropInScale*deltaTime, 1.0)
-        drawer.scale(dropScale, dropScale)
-        if (Controls.accelerate && Random.nextInt(1, 3) == 1) {
-            drawer.lineStrip(shipFlare)
-        }
-    }
-}
 
 fun move(spaceObject: SpaceObject, width: Double, height: Double, deltaTime: Double) {
     with (spaceObject) {
